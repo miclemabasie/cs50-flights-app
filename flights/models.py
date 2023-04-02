47b888f3c6637c9e933625291dee3dd5e89d4415
Miclem Abasie <miclemabasie3@gmail.com>
2023-04-02 17:32:58 +0100

    added airport model

--- flights/models.py
+++ flights/models.py
@@ -1,11 +1,26 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
 
 # Create your models here.
+class Airport(models.Model):
+    code = models.CharField(max_length=3)
+    city = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.city} ({self.code})"
+
 
 class Flight(models.Model):
-    origin = models.CharField(max_length=64)
-    destination = models.CharField(max_length=64)
+    pilot = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="departures")
+    destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="arrivals")
     duration = models.IntegerField()
 
-    
+    def __str__(self):
+
+        return f"<Flight: {self.id}: {self.origin} to {self.destination}>"
+
